f63e20ec40a4dacae82b84df48a6a1e0820c1ee7
nikushaomanidze <omanidze.nika@gmail.com>
2026-03-13 16:07:35 +0400

    add small change

--- src/components/NativeBottomSheet/NativeBottomSheet.tsx
+++ src/components/NativeBottomSheet/NativeBottomSheet.tsx
@@ -1,11 +1,3 @@
-import React from 'react'
-
-export type NativeBottomSheetProps = {
-  trigger: React.ReactNode
-  children: React.ReactNode
-  testID?: string
-}
-
 export const NativeBottomSheet = () => {
   throw new Error('NativeBottomSheet is not supported on web')
 }
