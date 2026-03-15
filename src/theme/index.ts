0274987417e6bffec3f3b161a0fcffd28e8b8048
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-03-15 21:25:03 +0400

    Export darkTheme and lightTheme from theme index.

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,3 +3,6 @@ export { rawTokens, type RawTokens } from './tokens.raw';
 export { ThemeType, type Theme, type ThemeColors } from './types';
 
 export { ThemeProvider, useTheme } from './ThemeContext';
+
+export { darkTheme } from './themes/dark';
+export { lightTheme } from './themes/light';
