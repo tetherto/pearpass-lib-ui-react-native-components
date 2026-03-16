21716e96b724f5e7891b2be5b7209b61f4967941
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-03-16 14:44:03 +0400

    Set explicit 12x12 size on strength indicator icons.

--- src/components/PasswordIndicator/PasswordIndicator.tsx
+++ src/components/PasswordIndicator/PasswordIndicator.tsx
@@ -32,7 +32,7 @@ export const PasswordIndicator = ({ variant, testID }: PasswordIndicatorProps): 
   return (
     <html.div data-testid={testID} style={styles.container}>
       <html.div style={styles.iconContainer} aria-hidden={true}>
-        <Icon color={color} />
+        <Icon width={12} height={12} color={color} />
       </html.div>
       <html.span style={[styles.label, variantStyleMap[variant]]}>
         {label}
