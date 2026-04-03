90feb8898eca1793b041753392ae7adca061cd49
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-04-03 14:29:33 +0400

    Fix Check icon to use currentColor for tintable fill.

--- src/components/ListItem/ListItem.native.tsx
+++ src/components/ListItem/ListItem.native.tsx
@@ -122,7 +122,7 @@ export const ListItem = React.forwardRef<View, ListItemProps>(function ListItem(
             }
           ]}
         >
-          {isSelected && <Check width={14} height={14} fill="white" />}
+          {isSelected && <Check width={14} height={14} color="white" />}
         </Pressable>
       )}
 
