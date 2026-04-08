e542d3f05a18160aaec01fe66ca58508f37869b4
ndrkltsk <aklitsuk@gmail.com>
2026-04-08 17:49:08 +0200

    refactor: update icon components to use 'currentColor' for fill property

--- src/icons/components/Check.native.tsx
+++ src/icons/components/Check.native.tsx
@@ -5,7 +5,7 @@ import type { SvgProps } from "react-native-svg";
 const SvgCheck = (props: SvgProps) => (
   <Svg width={24} height={24} fill="none" viewBox="0 0 14 14" {...props}>
     <Path
-      fill="#08090C"
+      fill="currentColor"
       d="M5.13 9.26 2.697 6.827l-.828.823 3.261 3.26 7-7-.823-.822z"
     />
   </Svg>
--- src/icons/components/Check.tsx
+++ src/icons/components/Check.tsx
@@ -4,7 +4,7 @@ import type { SVGProps } from "react";
 const SvgCheck = (props: SVGProps<SVGSVGElement>) => (
   <svg width="1em" height="1em" fill="none" viewBox="0 0 14 14" {...props}>
     <path
-      fill="#08090C"
+      fill="currentColor"
       d="M5.13 9.26 2.697 6.827l-.828.823 3.261 3.26 7-7-.823-.822z"
     />
   </svg>
