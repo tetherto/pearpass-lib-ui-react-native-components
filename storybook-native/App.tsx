83f76f4f6b37d34a934611902fc564443db600f6
ndrkltsk <aklitsuk@gmail.com>
2026-04-09 12:53:40 +0200

    feat: integrate custom fonts into Storybook app and add expo-font dependency

--- storybook-native/App.tsx
+++ storybook-native/App.tsx
@@ -1,1 +1,17 @@
-export { default } from './.rnstorybook';
+/* eslint-disable @typescript-eslint/no-require-imports */
+import React from 'react';
+import { useFonts } from 'expo-font';
+import StorybookUI from './.rnstorybook';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Humble Nostalgia': require('./assets/fonts/humble-nostalgia.otf'),
+    'Inter': require('./assets/fonts/inter.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <StorybookUI />;
+}
