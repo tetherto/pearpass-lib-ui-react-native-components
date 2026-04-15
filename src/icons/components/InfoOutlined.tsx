01218dc6208c394b95cb26f46fa688564d4706c2
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-04-15 16:06:53 +0400

    Add correct InfoOutlined icon.

--- src/icons/components/InfoOutlined.tsx
+++ src/icons/components/InfoOutlined.tsx
@@ -2,10 +2,10 @@
 import * as React from "react";
 import type { SVGProps } from "react";
 const SvgInfoOutlined = (props: SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+  <svg width="1em" height="1em" fill="none" viewBox="0 0 14 14" {...props}>
     <path
       fill="currentColor"
-      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m1 15h-2v-6h2zm0-8h-2V7h2z"
+      d="M6.417 4.085h1.166V5.25H6.417zm0 2.333h1.166v3.5H6.417zM7 1.168a5.835 5.835 0 0 0-5.833 5.833A5.835 5.835 0 0 0 7 12.835 5.835 5.835 0 0 0 12.833 7 5.836 5.836 0 0 0 7 1.168m0 10.5a4.673 4.673 0 0 1-4.667-4.667A4.673 4.673 0 0 1 7 2.335 4.673 4.673 0 0 1 11.666 7 4.673 4.673 0 0 1 7 11.668"
     />
   </svg>
 );
