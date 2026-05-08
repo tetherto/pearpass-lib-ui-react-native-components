566c3fdb0e25d254540525100906f41482746179
Tornike Kapanadze <tornikekapan@gmail.com>
2026-05-08 18:43:03 +0400

    Add TerminalTone icon and update HourglassBottom and WatchLater components to indicate auto-generation

---
+++ src/icons/components/TerminalTone.native.tsx
@@ -0,0 +1,23 @@
+// This file is auto-generated. Do not edit it.
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+import type { SvgProps } from "react-native-svg";
+const SvgTerminalTone = (props: SvgProps) => (
+  <Svg width={24} height={24} fill="none" viewBox="0 0 24 24" {...props}>
+    <Path
+      fill="currentColor"
+      d="M4 18h16V8H4zm8-3h6v2h-6zm-5.91-4.59L7.5 9l4 4-4 4-1.41-1.41L8.67 13z"
+      opacity={0.3}
+    />
+    <Path fill="currentColor" d="M18 15h-6v2h6z" />
+    <Path
+      fill="currentColor"
+      d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16c1.1 0 2-.9 2-2V6a2 2 0 0 0-2-2m0 14H4V8h16z"
+    />
+    <Path
+      fill="currentColor"
+      d="m7.5 17 4-4-4-4-1.41 1.41L8.67 13l-2.58 2.59z"
+    />
+  </Svg>
+);
+export default SvgTerminalTone;
---
+++ src/icons/components/TerminalTone.tsx
@@ -0,0 +1,22 @@
+// This file is auto-generated. Do not edit it.
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgTerminalTone = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M4 18h16V8H4zm8-3h6v2h-6zm-5.91-4.59L7.5 9l4 4-4 4-1.41-1.41L8.67 13z"
+      opacity={0.3}
+    />
+    <path fill="currentColor" d="M18 15h-6v2h6z" />
+    <path
+      fill="currentColor"
+      d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16c1.1 0 2-.9 2-2V6a2 2 0 0 0-2-2m0 14H4V8h16z"
+    />
+    <path
+      fill="currentColor"
+      d="m7.5 17 4-4-4-4-1.41 1.41L8.67 13l-2.58 2.59z"
+    />
+  </svg>
+);
+export default SvgTerminalTone;
