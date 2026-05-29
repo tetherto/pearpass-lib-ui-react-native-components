634b5030c96f3e4cd60210e92a6cd98534e575b9
giorgikh93 <giorgikhatiashvili93@yahoo.com>
2026-05-29 11:27:57 +0400

    fix test

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,19 @@ export default {
   },
   modulePaths: ['<rootDir>', '<rootDir>/node_modules'],
   moduleNameMapper: {
-    '^@tetherto/pearpass-lib-ui-theme-provider/native$': '<rootDir>/../pearpass-lib-ui-theme-provider/native/index.js',
-    '^@tetherto/pearpass-lib-ui-theme-provider$': '<rootDir>/../pearpass-lib-ui-theme-provider/src/index.js',
-    '^@tetherto/pearpass-utils-password-check$': '<rootDir>/../pearpass-utils-password-check/index.js'
+    '^react-native/(.+)': '<rootDir>/node_modules/react-native/$1',
+    '^react-native$': '<rootDir>/node_modules/react-native',
+    '^styled-components/native$':
+      '<rootDir>/node_modules/styled-components/native',
+    '^@tetherto/pearpass-lib-ui-theme-provider/native$':
+      '<rootDir>/../pearpass-lib-ui-theme-provider/native/index.js',
+    '^@tetherto/pearpass-lib-ui-theme-provider$':
+      '<rootDir>/../pearpass-lib-ui-theme-provider/src/index.js',
+    '^@tetherto/pearpass-utils-password-check$':
+      '<rootDir>/../pearpass-utils-password-check/index.js'
   },
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|@react-native-community|styled-components|@testing-library/react-native|pearpass-utils-password-check|pearpass-lib-ui-react-native-components)/)'
   ],
-  testMatch: [
-    '<rootDir>/src/**/*.test.{js,jsx,ts,tsx}'
-  ]
+  testMatch: ['<rootDir>/src/**/*.test.{js,jsx,ts,tsx}']
 }
